81fad75c3942821a5591d94ac287bfa95f0b9dc6
Andres Caicedo <andres.caicedo@okorum.com>
2022-10-04 16:11:53 -0500

    Tittle modified after deployment

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -5,7 +5,7 @@ import { ContactList } from './ContactList.jsx';
 //Contact: name, email, imageURL
 export const App = () => (
   <div>
-    <h1>Meteor Wallet</h1>
+    <h1>Meteor Wallet 2.0</h1>
     <ContactForm/>
     <ContactList/>
   </div>
